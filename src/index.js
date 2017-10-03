6d760d3e9da4d6a66cff8161427bece0d70754c1
Julie <julie.lin0812@gmail.com>
2017-10-03 13:18:13 -0700

    Changes bar graph chart to doughnut chart

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Chart from 'chart.js';
 
-
 document.getElementById('button').addEventListener('click', function() {
   let expensesData = [];
 
@@ -9,102 +8,79 @@ document.getElementById('button').addEventListener('click', function() {
   let cell =  parseFloat(document.getElementById("cell").value);
   let internet =  parseFloat(document.getElementById("internet").value);
   let fixedExpenses = rent + util + cell + internet;
-  expensesData.push(fixedExpenses);
+  // expensesData.push(fixedExpenses);
 
   let retirement =  parseFloat(document.getElementById("retirement").value);
   let savings =  parseFloat(document.getElementById("savings").value);
   let investments = retirement + savings;
-  expensesData.push(investments);
+  // expensesData.push(investments);
 
   let groceries =  parseFloat(document.getElementById("groceries").value);
   let hSupplies =  parseFloat(document.getElementById("hSupplies").value);
   let pSupplies =  parseFloat(document.getElementById("pSupplies").value);
   let transport =  parseFloat(document.getElementById("transport").value);
   let variableExpenses = groceries + hSupplies + pSupplies + transport;
-  expensesData.push(variableExpenses);
+  // expensesData.push(variableExpenses);
 
   let budget = parseFloat(document.getElementById("budget").value);
   let expenses = fixedExpenses + investments + variableExpenses;
   let excess = budget - expenses;
-  expensesData.push(excess);
+  // expensesData.push(excess);
 
   document.getElementById("excess").value = excess;
 
-  let ctx = document.getElementById("myChart");
-  let myChart = new Chart(ctx, {
-    type: 'bar',
+  let config = {
+    type: 'doughnut',
     data: {
-      labels: ["Fixed Expenses", "Investments/Savings", "Variable Expenses",
-        "Guilt-free Spending"],
       datasets: [{
-        label: 'Expenses Total',
-        data: expensesData,
+        label: 'Expenses',
+        data: [
+          rent,
+          util,
+          cell,
+          internet,
+          retirement,
+          savings,
+          groceries,
+          hSupplies,
+          pSupplies,
+          transport,
+          excess
+        ],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
+          'rgb(255, 128, 128)',
+          'rgb(255,0,0)',
+          'rgb(255,165,0)',
+          'rgb(255,255,0)',
+          'rgb(128, 255, 191)',
+          'rgb(0,255,0)',
+          'rgb(51, 153, 255)',
+          'rgb(0, 0, 255)',
+          'rgb(128, 128, 255)',
+          'rgb(102, 0, 204)',
+          'rgb(128, 0, 128)'
+        ]
+      }
+    ],
+      // labels: ["Fixed", "Investments", "Variable", "Excess"]
+      labels: ["rent", "util", "cell", "internet", "retirement", "savings", "groceries", "hSupplies", "pSupplies", "transport", "excess"]
     },
     options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
+      responsive: true,
+      legend: {
+        position: 'top'
+      },
+      title: {
+        display: true,
+        text: 'Doughtnut Chart Test'
+      },
+      animation: {
+        animateScale: true,
+        animateRotate: true
       }
     }
-  });
+  };
+
+  let ctx = document.getElementById("myChart");
+  let myChart = new Chart(ctx, config);
 });
-
-
-// var ctx = document.getElementById("myChart");
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
